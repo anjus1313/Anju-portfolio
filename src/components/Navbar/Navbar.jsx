f7148202abc19a608a471e8ed3a89f7b18e8a330
anjus <anjuskumar1313@gmail.com>
2024-10-19 18:16:30 -0400

    Made navbar responsive

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 
 import styles from './Navbar.module.css';
+import {getImageURL} from '../../utils';
 
 export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
     return <div>
         <nav className={styles.navbar}>
             <a className={styles.title} href="/">Portfolio</a>
             <div className={styles.menu}>
-                <ul className={styles.menuItems}>
+                <img 
+                    className={styles.menuBtn} 
+                    src={
+                        menuOpen 
+                        ? getImageURL("nav/closeIcon.png") 
+                        : getImageURL("nav/menuIcon.png")
+                    }
+                    alt="menu-button"
+                    onClick={() => setMenuOpen(!menuOpen)}
+                />
+                <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+                    onClick={() => setMenuOpen(false)}
+                    >
                     <li>
                         <a href="#about">About</a>
                     </li>
