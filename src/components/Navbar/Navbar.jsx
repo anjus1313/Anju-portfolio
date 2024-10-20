395e47ae96054690ecb5c3c1771152c1bef162cd
anjus <anjuskumar1313@gmail.com>
2024-10-19 20:54:54 -0400

    Corrected navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 import styles from './Navbar.module.css';
-import {getImageURL} from '../../utils';
+import { getImageURL } from '../../utils';
 
 export const Navbar = () => {
     const [menuOpen, setMenuOpen] = useState(false);
@@ -10,19 +10,19 @@ export const Navbar = () => {
         <nav className={styles.navbar}>
             <a className={styles.title} href="/">Portfolio</a>
             <div className={styles.menu}>
-                <img 
-                    className={styles.menuBtn} 
+                <img
+                    className={styles.menuBtn}
                     src={
-                        menuOpen 
-                        ? getImageURL("nav/closeIcon.png") 
-                        : getImageURL("nav/menuIcon.png")
+                        menuOpen
+                            ? getImageURL("nav/closeIcon.png")
+                            : getImageURL("nav/menuIcon.png")
                     }
                     alt="menu-button"
                     onClick={() => setMenuOpen(!menuOpen)}
                 />
                 <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
                     onClick={() => setMenuOpen(false)}
-                    >
+                >
                     <li>
                         <a href="#about">About</a>
                     </li>
